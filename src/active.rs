46dcbc9d715a0bf49e46cfa0a7e56cbaeb70ac7d
Probably Winston Churchill <nicolas.p.ammann@gmail.com>
2021-10-02 11:52:37 -0400

    Fixed issues in active discovery methods

--- src/active.rs
+++ src/active.rs
@@ -10,25 +10,72 @@ use crate::log::{
 };
 
 /// defines the maximum recursion depth for looking for domains
-const MAX_DEPTH: u16 = 5;
+const MAX_DEPTH: u16 = 1;
 
-/// finds unique subdomains given a base host and list of domains
-fn find_subdomains(host: Url, domains: Vec<Url>) -> Vec<Url> {
-    let mut ret: Vec<&str> = Vec::new();
-    // loop through each domain
-    for domain in domains {
-        // filter off the domains that aren't
-        let domain_host_str = match domain.host_str() {
-            Some(a) => a.clone(),
+/// helper function that adds `https://` to the front of a URL
+fn https(url: String) -> String {
+    "https://".to_string() + &url
+}
+
+/// removes duplicate domains from a vector
+fn clear_dupes(out: Vec<Url>) -> Vec<Url> {
+    let mut ret: Vec<String> = Vec::new();
+
+    // loop over each domain
+    for v in out {
+        let domain_host_str = match v.host_str() {
+            Some(a) => a.to_string(),
             None => {
                 log(
                     LogType::LogWarn, 
                     format!("Invalid URL detected. Skipping")
                 );
-                return Vec::new();
+                continue;
             }
         };
-        let domain_host_str = domain_host_str.clone();
+        // now make sure we don't have that domain already
+        if !ret.contains(&domain_host_str) {
+            ret.push(domain_host_str.clone());
+        }   
+    }
+
+    // now convert them back to URLs
+    let mut uniq: Vec<Url> = Vec::new();
+    for u in ret {
+        match Url::parse(&https(u.clone())[..]) {
+            Ok(a) => uniq.push(a),
+            Err(e) => {
+                log(
+                    LogType::LogErr, 
+                    format!(
+                        "Failed to parse URL {}: {}",
+                        u,
+                        e
+                    )
+                )
+            }
+        };
+    }
+
+    uniq
+}
+
+/// finds unique subdomains given a base host and list of domains
+fn find_subdomains(host: Url, domains: Vec<Url>) -> Vec<Url> {
+    let mut ret: Vec<String> = Vec::new();
+    // loop through each domain
+    for domain in domains {
+        // filter off the domains that aren't
+        let domain_host_str = match domain.host_str() {
+            Some(a) => a.to_string(),
+            None => {
+                log(
+                    LogType::LogWarn, 
+                    format!("Invalid URL detected. Skipping")
+                );
+                continue;
+            }
+        };
         let host_str = match host.host_str() {
             Some(a) => a,
             None => {
@@ -43,16 +90,27 @@ fn find_subdomains(host: Url, domains: Vec<Url>) -> Vec<Url> {
         if let Some(_) = domain_host_str.find(host_str) {
             // now make sure we don't have that domain already
             if !ret.contains(&domain_host_str) {
-                ret.push(domain_host_str);
-            }
-            
+                ret.push(domain_host_str.clone());
+            }   
         }
     }
 
     // convert unique subdomains to URLs
     let mut uniq: Vec<Url> = Vec::new();
     for u in ret {
-        uniq.push(Url::parse(u).unwrap());
+        match Url::parse(&https(u.clone())[..]) {
+            Ok(a) => uniq.push(a),
+            Err(e) => {
+                log(
+                    LogType::LogErr, 
+                    format!(
+                        "Failed to parse URL {}: {}",
+                        u,
+                        e
+                    )
+                )
+            }
+        };
     }
     
     uniq
@@ -89,7 +147,7 @@ pub fn subdomains(
     url: Url, 
     depth: u16, 
     client: Option<reqwest::blocking::Client>,
-    stats: Option<Stats>
+    stats: Option<&mut Stats>
 ) -> Vec<Url> {
     //println!("URL={}, Depth={}", url, depth);
     // see if we are at the max depth to stop looking for crap
@@ -107,9 +165,9 @@ pub fn subdomains(
             .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0")
             .build().unwrap();
     }
-    let s: Stats;
+    let mut s: Stats;
     if let Some(x) = stats {
-        s = x;
+        s = *x;
     } else {
         s = Stats {urls: 0};
     }
@@ -144,9 +202,10 @@ pub fn subdomains(
 
     // search the contents for URLS and get them
     let t = search_body(body);
-    //for s in t.clone() {
+    for _ in t.clone() {
         //println!("{}",s);
-    //}
+        s.urls += 1;
+    }
     log(
         LogType::LogInfo,
         format!(
@@ -164,12 +223,14 @@ pub fn subdomains(
         )
     );*/
     for sub in find_subdomains(url, t) {
-        ret.append(&mut subdomains(sub, 
+        ret.push(sub.clone());
+        let mut out = subdomains(sub, 
             depth + 1, 
             Some(r.clone()),
-            Some(s)
-        ));
+            Some(&mut s)
+        );
+        ret.append(&mut out);
     }
-    //ret.append(&mut find_subdomains(url, t));
-    ret
+
+    clear_dupes(ret)
 }
